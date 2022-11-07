107df774a045af5e48bc28553f2f35ac21d5c2b1
Christopher Carson <chris@nowzoo.com>
2022-11-07 16:32:57 -0600

    No need to have env vars because the translation directory has to be hard coded

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,28 +1,3 @@
-const PUBLIC_I18N_DEFAULT_LOCALE = 'PUBLIC_I18N_DEFAULT_LOCALE';
-const PUBLIC_I18N_SUPPORTED_LOCALES = 'PUBLIC_I18N_SUPPORTED_LOCALES';
-export type I18NEnv = {
-  defaultLocale: string;
-  supportedLocales: string[];
-};
-
-export const validateEnvVariables = (env: Record<string, string>): I18NEnv => {
-  const defaultLocale = env[PUBLIC_I18N_DEFAULT_LOCALE];
-  if (typeof defaultLocale !== 'string' || defaultLocale.length === 0) {
-    throw new Error(
-      `The default locale must be defined in your environment as PUBLIC_I18N_DEFAULT_LOCALE. ` +
-        `For example, put PUBLIC_I18N_DEFAULT_LOCALE=en-us in your .env file. `
-    );
-  }
-  let supportedLocales: string[] = [];
-  if (typeof env[PUBLIC_I18N_SUPPORTED_LOCALES] === 'string') {
-    supportedLocales = env[PUBLIC_I18N_SUPPORTED_LOCALES].split(',')
-      .map((s) => s.trim())
-      .filter((s) => s.length === 0);
-  }
-
-  return { defaultLocale, supportedLocales };
-};
-
 export const parseMessageId = (
   id: string
 ): { messageBagId: string; messageKey: string } => {
