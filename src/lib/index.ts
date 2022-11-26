fc0e895af87df2bb52010c1aa8c26e0491272933
Christopher Carson <chris@nowzoo.com>
2022-11-26 11:27:10 -0600

    added loadLocale

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,5 @@
 export type MessageBag = {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: MessageBag | string | ((...args: any) => string);
+  [key: string]: MessageBag | string | ((...args: never[]) => string);
 };
 import { TRANSLATIONS_FILE_NAME } from './_cli/constants';
 
@@ -10,10 +9,13 @@ const moduleLoadFns: Record<string, () => Promise<Record<string, unknown>>> =
   }) as Record<string, () => Promise<Record<string, unknown>>>;
 const loadedMessageBags: Record<string, Record<string, unknown>> = {};
 
-const mergeDeep = <T extends MessageBag>(translated: Partial<T>, original: T): T => {
-  const result: T = {...original}
+const mergeDeep = <T extends MessageBag>(
+  translated: Partial<T>,
+  original: T
+): T => {
+  const result: T = { ...original };
   Object.keys(original).forEach((key: keyof T) => {
-    const value = original[key]
+    const value = original[key];
     if (typeof value === 'function') {
       if (typeof translated[key] === 'function') {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -29,31 +31,41 @@ const mergeDeep = <T extends MessageBag>(translated: Partial<T>, original: T): T
       return;
     }
     if (Object.prototype.toString.call(value) === '[object Object]') {
-      if (Object.prototype.toString.call(translated[key]) === '[object Object]') {
+      if (
+        Object.prototype.toString.call(translated[key]) === '[object Object]'
+      ) {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        result[key] = mergeDeep(translated[key] as any, value) as any
+        result[key] = mergeDeep(translated[key] as any, value) as any;
       }
     }
-  })
+  });
   return result;
-}
+};
+
+export const loadLocale = async (locale: string) => {
+  const endsWith = `${TRANSLATIONS_FILE_NAME}.${locale}.ts`;
+  const filePaths: string[] = Object.keys(moduleLoadFns).filter((s) =>
+    s.endsWith(endsWith)
+  );
+  const promises = filePaths.map((filePath) => {
+    return moduleLoadFns[filePath]().then((mod) => {
+      loadedMessageBags[filePath] = mod;
+    });
+  });
+  await Promise.all(promises)
+};
 
 export const defineMessageBag = <T extends MessageBag>(
   messageBagId: string,
-  messageBag: T
-): ((locale?: string) => Promise<T>) => {
-  const promise = async (locale?: string) => {
-    if (!locale) {
-      return messageBag;
-    }
-    const path = `/src/i18n/${messageBagId}/${TRANSLATIONS_FILE_NAME}.${locale}.ts`;
-    if (!moduleLoadFns[path]) {
-      return messageBag;
-    }
-    if (!loadedMessageBags[path]) {
-      loadedMessageBags[path] = await moduleLoadFns[path]();
-    }
-    return mergeDeep((loadedMessageBags[path] || {}) as any, messageBag)
-  };
-  return promise;
+  messageBag: T,
+  locale?: string
+): T => {
+  if (!locale) {
+    return messageBag;
+  }
+  const path = `/src/i18n/${messageBagId}/${TRANSLATIONS_FILE_NAME}.${locale}.ts`;
+  if (loadedMessageBags[path]) {
+    return mergeDeep((loadedMessageBags[path] || {}) as Partial<T>, messageBag);
+  }
+  return messageBag;
 };
