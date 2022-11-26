2742d485edb6c785e5af998c87d72105b5799356
Christopher Carson <chris@nowzoo.com>
2022-11-26 08:41:24 -0600

    examples

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,6 @@
 import { localePickerMessages} from './messages';
 import type { LayoutLoad } from './$types'
 export const load: LayoutLoad = async (event) => {
-  
   return {
     ...(event.data),
     localePickerMessages: await localePickerMessages(event.data.locale)
